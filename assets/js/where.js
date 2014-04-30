bb54694cb6d2bfb96657005fc93a1b397d7ea3f0
Michelle <mhertzfeld@gmail.com>
2014-04-30 19:16:19 -0400

    Update remained of image links

--- assets/js/where.js
+++ assets/js/where.js
@@ -5,6 +5,7 @@ var where_stats_data = {
 	//Tooltips 
 	"statsOffshore" : [ 
 		//Remember that the count starts at zero 
+		//NOTE: img links are currently hard coded to gh pages site -- fix later -- figure out how to make these relative given that Jekyll won't parse JS files and so can't use {{ site.url }} 
 		{ 
 			//Array ID -> 0 
 			"Title" : "U.S. Treasury", 
@@ -24,17 +25,17 @@ var where_stats_data = {
 			//Array ID -> 2
 			"Title" : "Historic Preservation Fund", 
 			"Content" : "The <a href=\"http\://www.nps.gov/history/hpg/\">Historic Preservation Fund</a> helps preserve U.S. historical and archaeological sites and cultural heritage through grants to State and Tribal Historic Preservation Offices. Some examples of activities include:", 
-			"Img1" : "<img src=\"/assets/img/icon_1566.svg\" alt=\"City buildings\"><h3><a href=\"http\://www.michiganmodern.org/\">Survey Modernist Architecture, Michigan</a></h3>",
-			"Img2" : "<img src=\"/assets/img/icon_10119.svg\" alt=\"Schoolhouse\"><h3><a href=\"http\://ncptt.nps.gov/blog/tribal-heritage-grants/\">Restore Peoria Schoolhouse, Peoria Tribe of Indians, Oklahoma</a></h3>",
-			"Img3" : "<img src=\"/assets/img/icon_7038.svg\" alt=\"Video camera\"><h3><a href=\"http\://ncptt.nps.gov/blog/tribal-heritage-grants/\">Document Yup’ik Songs & Dances, Calista Elders Council of Alaska</a></h3>"
+			"Img1" : "<img src=\"http://meiqimichelle.github.io//assets/img/icon_1566.svg\" alt=\"City buildings\"><h3><a href=\"http\://www.michiganmodern.org/\">Survey Modernist Architecture, Michigan</a></h3>",
+			"Img2" : "<img src=\"http://meiqimichelle.github.io//assets/img/icon_10119.svg\" alt=\"Schoolhouse\"><h3><a href=\"http\://ncptt.nps.gov/blog/tribal-heritage-grants/\">Restore Peoria Schoolhouse, Peoria Tribe of Indians, Oklahoma</a></h3>",
+			"Img3" : "<img src=\"http://meiqimichelle.github.io//assets/img/icon_7038.svg\" alt=\"Video camera\"><h3><a href=\"http\://ncptt.nps.gov/blog/tribal-heritage-grants/\">Document Yup’ik Songs & Dances, Calista Elders Council of Alaska</a></h3>"
 		},
 		{ 	
 			//Array ID -> 3
 			"Title" : "Land & Water Conservation Fund", 
 			"Content" : "The <a href=\"http\://www.nps.gov/lwcf/\">Land & Water Conservation Fund</a> program provides matching grants to states and local governments for the acquisition and development of public outdoor recreation areas. </p><p>Here are a few places that were funded by LWCF grants:", 
-			"Img1" : "<img src=\"/assets/img/icon_16251.svg\" alt=\"Mountains\"><h3><a href=\"http\://www.emnrd.state.nm.us/SPD/eaglenestlakestatepark.html\">Eagle Nest Lake State Park, New Mexico</a></h3>",
-			"Img2" : "<img src=\"/assets/img/icon_26235.svg\" alt=\"Playground\"><h3><a href=\"http\://www.mitchellparkdc.org/history.html\">Mitchell Park, District of Columbia</a></h3>",
-			"Img3" : "<img src=\"/assets/img/icon_25079.svg\" alt=\"Baseball field\"><h3><a href=\"http\://www.ofallon.org/parks/pages/family-sports-park\">Family Sports Park, Illinois</a></h3>"
+			"Img1" : "<img src=\"http://meiqimichelle.github.io//assets/img/icon_16251.svg\" alt=\"Mountains\"><h3><a href=\"http\://www.emnrd.state.nm.us/SPD/eaglenestlakestatepark.html\">Eagle Nest Lake State Park, New Mexico</a></h3>",
+			"Img2" : "<img src=\"http://meiqimichelle.github.io//assets/img/icon_26235.svg\" alt=\"Playground\"><h3><a href=\"http\://www.mitchellparkdc.org/history.html\">Mitchell Park, District of Columbia</a></h3>",
+			"Img3" : "<img src=\"http://meiqimichelle.github.io//assets/img/icon_25079.svg\" alt=\"Baseball field\"><h3><a href=\"http\://www.ofallon.org/parks/pages/family-sports-park\">Family Sports Park, Illinois</a></h3>"
 		}, 
 	] 
 }
